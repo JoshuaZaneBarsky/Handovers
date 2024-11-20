6584f133d256a4330fa02987de6688a0798fbdf5
JoshuaZaneBarsky <joshuazanebarsky@gmail.com>
2024-11-20 14:30:08 -0800

    NetworkData.py added

---
+++ NetworkData.py
@@ -0,0 +1,14 @@
+class NetworkData():
+    def __init__(self,network_data):
+        self.network_data = network_data #pseudo line
+        # tower names
+        # device names
+        # device-tower connections
+        # device-tower ranges
+
+
+        # analytics? RSPQ? ...
+
+        # Past inactive connections
+
+        
